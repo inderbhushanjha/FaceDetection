d539b8276d5c5450c77547bafc8f69615f4de285
Inder Bhushan Jha <47220493+inderbhushanjha@users.noreply.github.com>
2020-01-11 11:46:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ else
 	apt install libxvidcore-dev libx264-dev -y
 	apt install libatlas-base-dev gfortran -y
 	apt install install python2.7-dev python3-dev -y
+	pip install opencv-python
 	cd ~
 	cv="https://github.com/Itseez/opencv/archive/$ver.zip"
 	echo $cv
@@ -33,6 +34,6 @@ else
 	make -j4
 	make install
 fi
-echo -e "\e[44m\e[5mThank You for Using this tool...\e[0m"
+echo -e "\e[44m\e[5mThank You for Using this tool for more tools and tricks visit http://github.com/inderbhushanjha...\e[0m"
 
 
