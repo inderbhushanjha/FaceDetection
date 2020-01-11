b08d9c0f839cf27c33b7e12fb3eefda8af05587c
Inder Bhushan Jha <47220493+inderbhushanjha@users.noreply.github.com>
2020-01-11 20:10:20 +0530

    Update importcv2.py

--- importcv2.py
+++ importcv2.py
@@ -1,15 +1,13 @@
 import cv2
 
-# Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# To capture video from webcam. 
 cap = cv2.VideoCapture(0)
 # To use a video file as input 
 # cap = cv2.VideoCapture('filename.mp4')
 
 while True:
-    # Read the frame
+  
     _, img = cap.read()
 
     # Convert to grayscale
